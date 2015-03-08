c0830c1c5d25bd965e502f8f88d6e43447c15c3d
H Kellaway <hkellaway.tech@gmail.com>
2015-03-08 15:45:40 -0700

    Creates Today Widge target

---
+++ refuge-ios/Today Widget/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Today Widget
+//
+//  Created by Harlan Kellaway on 3/8/15.
+//  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
