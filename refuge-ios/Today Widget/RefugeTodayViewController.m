c0d3c3a8896e03169f7c7d633f3110092886077e
H Kellaway <hkellaway.tech@gmail.com>
2015-03-08 16:23:58 -0700

    Starts implementing display logic

--- refuge-ios/Today Widget/RefugeTodayViewController.m
+++ refuge-ios/Today Widget/RefugeTodayViewController.m
@@ -6,12 +6,16 @@
 //  Copyright (c) 2015 Refuge Restrooms. All rights reserved.
 //
 
-#import "RefugeTodayViewController.h"
 #import <NotificationCenter/NotificationCenter.h>
+#import "RefugeTodayViewController.h"
+#import "RefugeRestroom.h"
 
 static CGFloat const kMarginSize = 10.0;
 
 @interface RefugeTodayViewController () <NCWidgetProviding>
+
+@property (weak, nonatomic) NSMutableArray *closestRestrooms;
+@property (weak, nonatomic) IBOutlet UILabel *restroomNameLabel;
 
 @end
 
@@ -22,18 +26,25 @@ static CGFloat const kMarginSize = 10.0;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [self updateInterface];
 }
 
 #pragma mark - Protocol conformance
 
-#pragma mark NCWidget Providing
+#pragma mark NCWidgetProviding
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
-    // Perform any setup necessary in order to update the view.
+    
+    [self fetchClosestRestrooms];
     
     // If an error is encountered, use NCUpdateResultFailed
+    
     // If there's no update required, use NCUpdateResultNoData
+    
+    
     // If there's an update, use NCUpdateResultNewData
+    [self updateInterface];
 
     completionHandler(NCUpdateResultNewData);
 }
@@ -42,6 +53,28 @@ static CGFloat const kMarginSize = 10.0;
 {
     defaultMarginInsets.bottom = kMarginSize;
     return defaultMarginInsets;
+}
+
+#pragma mark - Helpers
+
+- (void)updateInterface
+{
+    self.restroomNameLabel.text = @"Test";
+}
+
+- (void)fetchClosestRestrooms
+{
+    RefugeRestroom *restroom1 = [[RefugeRestroom alloc] init];
+    restroom1.name = @"Test Restroom 1";
+    RefugeRestroom *restroom2 = [[RefugeRestroom alloc] init];
+    restroom2.name = @"Test Restroom 2";
+    RefugeRestroom *restroom3 = [[RefugeRestroom alloc] init];
+    restroom3.name = @"Test Restroom 3";
+    
+    self.closestRestrooms = [NSMutableArray array];
+    [self.closestRestrooms addObject:restroom1];
+    [self.closestRestrooms addObject:restroom2];
+    [self.closestRestrooms addObject:restroom3];
 }
 
 @end
